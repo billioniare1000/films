34c34377e119546503f39f224295827bd210e2e8
billioniare1000 <zmo311342@gmail.com>
2025-02-09 13:39:49 +0200

    push 2

--- src/netlify/functions/tmdb.js
+++ src/netlify/functions/tmdb.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event,  ) => {
   const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjEyYjFkNTg4YzBhZmY5YzI4MTcyMjlhMGY1MDIyZSIsIm5iZiI6MTczODgzNjk1My42OTIsInN1YiI6IjY3YTQ4YmQ5ODg1OGM4NzA1YTY2ZTFjMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WNo8h9snlnLbQb3y27afYIRn5s9Kgc4Y2yF7DH4Raq4';
-  const BASE_URL = 'https://inquisitive-banoffee-5b24b3.netlify.app/.netlify/functions/tmdb';
+  const BASE_URL = 'https://api.themoviedb.org/3';
 
   const query = event.queryStringParameters.query || '';
   const endpoint = query
